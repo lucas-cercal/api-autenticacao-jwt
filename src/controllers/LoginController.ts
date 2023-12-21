c68b2940c0cac796c0381ba695ecbf7388377fff
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-12-21 10:01:12 -0300

    feat: add authMiddleware

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -1,12 +1,8 @@
 import { Request, Response } from 'express'
 import { userRepository } from '../repositories/userRepository'
-import { BadRequestError, UnauthorizedError } from '../helpers/api-erros'
+import { BadRequestError } from '../helpers/api-erros'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
-
-type JwtPayload = {
-  id: number
-}
 
 export class LoginController {
   async login(req: Request, res: Response) {
@@ -33,20 +29,6 @@ export class LoginController {
   }
 
   async getProfile(req: Request, res: Response) {
-    const { authorization } = req.headers
-
-    if (!authorization) throw new UnauthorizedError('Não autorizado')
-
-    const token = authorization.split(' ')[1]
-
-    const { id } = jwt.verify(token, process.env.JWT_PASS ?? '') as JwtPayload
-
-    const user = await userRepository.findOneBy({ id })
-
-    if (!user) throw new UnauthorizedError('Não autorizado')
-
-    const { password: _, ...loggedUser } = user
-
-    return res.json(loggedUser)
+    return res.json(req.user)
   }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express'
 import { UserController } from './controllers/UserController'
 import { LoginController } from './controllers/LoginController'
+import { authMiddleware } from './middlewares/authMiddleware'
 
 const routes = Router()
 
 routes.post('/user', new UserController().create)
 routes.post('/login', new LoginController().login)
+
+routes.use(authMiddleware)
+
 routes.get('/profile', new LoginController().getProfile)
 
 export default routes
